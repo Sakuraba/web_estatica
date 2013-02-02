35afef2a073c831feb9559758e46d321da3800e8
Sakuraba <davidrm@ubuntu.(none)>
2013-02-02 09:41:59 -0800

    Mi primer jakertraker

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebEstatica::Application.config.secret_token = '831f0919577edd874b2c67b91c139cd1f6a94c22c45c6d6c2436e1e277294b3ae265578dc6a5fdb64e16d645832b9bfb8280f087446c84568a7244f82854b7ab'
